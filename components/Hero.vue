bea23b782986bb25ca283614a7e59fa5d351d8d2
JackHughes03 <jwhughes03@yahoo.com>
2024-11-25 13:28:23 +0000

    Updated hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,20 +1,58 @@
+<script>
+import { onMounted } from 'vue';
+
+export default {
+    data() {
+        return {
+            isMenuOpen: false
+        }
+    },
+
+    methods: {
+        toggleMenu() {
+            const icon = document.querySelector('.icon');
+            const nav = document.querySelector('nav');
+            icon.classList.toggle("open");
+
+            if (icon.classList.contains('open')) {
+                nav.classList.add('open');
+            } else {
+                nav.classList.remove('open');
+            }
+        }
+    }
+}
+</script>
+
 <template>
     <div>
-        <nav class="bg-primary shadow-md fixed w-full z-50 h-14">
-            <ul class="flex items-center justify-between px-7 gap-8 text-white/85 font-light py-4 mx-auto w-full">
-                <div class="items-center gap-8 hidden md:flex">
-                    <li><a href="#"
-                            class="hover:text-white transition-colors duration-200">Home</a>
+        <nav class="shadow-md bg-primary fixed w-full duration-300 z-50 h-14">
+
+            <button @click="toggleMenu" class="icon sm:!hidden flex nav-icon-2">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+            <ul class="hidden flex-col h-full items-center justify-center text-xl gap-4 text-white mobile-nav">
+                <li><a href="#" class="mobile-nav-link">Home</a></li>
+                <li><a href="#" class="mobile-nav-link">Dyslexia &
+                        Dyscalculia</a></li>
+                <li><a href="#" class="mobile-nav-link">Assessments</a></li>
+                <li><a href="#" class="mobile-nav-link">Contact</a></li>
+            </ul>
+
+            <ul
+                class="hidden sm:flex items-center justify-between px-7 gap-8 text-white/85 font-light py-4 mx-auto w-full">
+                <div class="items-center gap-8 flex">
+                    <li><a href="#" class="hover:text-white transition-colors duration-200">Home</a>
                     </li>
-                    <li><a href="#"
-                            class="hover:text-white transition-colors duration-200">Dyslexia &
+                    <li><a href="#" class="hover:text-white transition-colors duration-200">Dyslexia &
                             Dyscalculia</a></li>
-                    <li><a href="#"
-                            class="hover:text-white transition-colors duration-200">Assessments</a>
+                    <li><a href="#" class="hover:text-white transition-colors duration-200">Assessments</a>
                     </li>
                 </div>
-                <li class="hidden md:flex"><a href="#"
-                        class="hover:text-white transition-colors duration-200">Contact</a>
+                <li class="flex"><a href="#" class="hover:text-white transition-colors duration-200">Contact</a>
                 </li>
             </ul>
         </nav>
@@ -42,9 +80,132 @@
 
 <style>
 header {
-    background-image: url('@/assets/images/girl-writing.webp');
+    background-image: url('img/girl-writing.webp');
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+}
+
+.nav-icon-2 {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    cursor: pointer;
+    display: inline-block;
+}
+
+.nav-icon-2 span {
+    background-color: #FFF;
+    position: absolute;
+    border-radius: 2px;
+    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
+}
+
+.nav-icon-2 span:nth-child(1) {
+    width: 100%;
+    height: 3px;
+    display: block;
+    top: 0px;
+    left: 0px;
+}
+
+.nav-icon-2 span:nth-child(2) {
+    width: 100%;
+    height: 3px;
+    display: block;
+    top: 10px;
+    left: 0px;
+}
+
+.nav-icon-2 span:nth-child(3) {
+    width: 100%;
+    height: 3px;
+    display: block;
+    bottom: 7px;
+    left: 0px;
+}
+
+.nav-icon-2:not(.open):hover span:nth-child(1) {
+    width: 100%;
+    height: 3px;
+    display: block;
+    top: 0px;
+    left: 0px;
+    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
+}
+
+.nav-icon-2:not(.open):hover span:nth-child(2) {
+    width: 100%;
+    height: 3px;
+    display: block;
+    top: 10px;
+    left: 0px;
+    transition: .4s cubic-bezier(.8, .5, .2, 1.4);
+}
+
+.nav-icon-2:not(.open):hover span:nth-child(3) {
+    width: 100%;
+    height: 3px;
+    display: block;
+    bottom: 7px;
+    left: 0px;
+    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
+}
+
+.nav-icon-2.open span:nth-child(1) {
+    left: 5px;
+    top: 7px;
+    width: 26px;
+    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
+    transform: rotate(90deg);
+    transition-delay: 150ms;
+}
+
+.nav-icon-2.open span:nth-child(2) {
+    left: 2px;
+    top: 15px;
+    width: 20px;
+    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
+    transform: rotate(45deg);
+    transition-delay: 50ms;
+}
+
+.nav-icon-2.open span:nth-child(3) {
+    left: 14px;
+    top: 15px;
+    width: 20px;
+    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
+    transform: rotate(-45deg);
+    transition-delay: 100ms;
+}
+
+nav.open {
+    height: 90%;
+}
+
+nav.open .mobile-nav {
+    display: flex;
+}
+
+.mobile-nav-link {
+    position: relative;
+    padding: 2px 4px;
+}
+
+.mobile-nav-link::after {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 2px;
+    bottom: -2px;
+    left: 0;
+    background-color: white;
+    transition: width 0.3s ease;
+}
+
+.mobile-nav-link:hover::after {
+    width: 100%;
 }
 </style>
